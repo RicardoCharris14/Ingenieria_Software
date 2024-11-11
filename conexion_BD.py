7b77b7daf2af72cee87145f18fd3ab98e15f6ba2
Your Name <you@example.com>
2024-11-10 22:50:11 -0300

    Creacion de la tabla Especialista y la tabla Horario_Atencion

--- conexion_BD.py
+++ conexion_BD.py
@@ -4,13 +4,34 @@ try:
     connection = sqlite3.connect("Database/bd")
     cursor = connection.cursor()
 
+    cursor.executescript("""
+        BEGIN;
+        CREATE TABLE Especialista(
+            nombre TEXT NOT NULL,
+            especialidad TEXT NO NULL,
+            rut TEXT NOT NULL,
+            telefono INTEGER,
+            correo TEXT NOT NULL);
+
+        CREATE TABLE Horario_Atencion(
+            id INTEGER PRIMARY KEY,
+            dia TEXT NOT NULL,
+            inicio INTEGER,
+            fin INTEGER, 
+            valor INTEGER,
+            disponible INTEGER,
+            especialista TEXT NOT NULL,
+            FOREIGN KEY (especialista) REFERENCES Especialista(nombre));
+        COMMIT;
+    """)
+
     cursor.execute("""
     CREATE TABLE calendario_semanal (
         fecha_inicio TEXT NOT NULL,
         fecha_termino TEXT NOT NULL,
         rut_especialista TEXT NOT NULL
         PRIMARY KEY (fecha_inicio, fecha_termino),
-        FOREIGN KEY (rut_especialista) REFERENCES especialista(rut)
+        FOREIGN KEY (rut_especialista) REFERENCES Especialista(rut)
     )
     """)
 
