ad2d62226d054201e596b24bbd90198d3a2be419
Matfigueroa20 <matfigueroa2023@udec.cl>
2024-11-10 22:25:49 -0300

    Se crean tablas prevision y prevision_especialista en la base de datos

--- conexion_BD.py
+++ conexion_BD.py
@@ -14,6 +14,24 @@ try:
     )
     """)
 
+    # Crear tabla prevision
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS prevision (
+        nombre TEXT PRIMARY KEY NOT NULL
+    )
+    """)
+
+    # Crear tabla prevision_especialista
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS prevision_especialista (
+        nombre_prevision TEXT NOT NULL,
+        rut_especialista TEXT NOT NULL,
+        FOREIGN KEY (nombre_prevision) REFERENCES prevision(nombre),
+        FOREIGN KEY (rut_especialista) REFERENCES especialista(rut),
+        PRIMARY KEY (nombre_prevision, rut_especialista)
+    )
+    """)
+
     connection.commit() #Guarda los cambios en la BD
 
 except Exception as ex:
